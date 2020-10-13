24466d55a9b91e68938bc51e70d84c38a1a79dc7
anavianm <anavian.max@gmail.com>
2020-10-13 16:17:50 -0400

    new scene and moving enemy towards player

---
+++ Assets/Scripts/MoveRandom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRandom : MonoBehaviour
+{
+    private GameObject playerObj = null;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerObj == null) {
+            playerObj = GameObject.Find("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, playerObj.transform.position, step);
+
+        Debug.Log("Player Position is: " + playerObj.transform.position);
+    }
+}
