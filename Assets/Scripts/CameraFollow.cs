f570639ed1b5a156de22e469561c0265dfbe683b
LeoIM <leo@leoim.com>
2020-10-18 17:51:42 -0700

    New Camera System + Navmesh/AI Tweaking
    Added a simpler to control camera system for the player. Fixed some navmesh issues that were creating broken pathing. Slowed down the enemy to make it more interesting to navigate around.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject character;
+    public Vector3 cameraOffset = new Vector3(10, 10, 10);
+    public float cameraSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = character.transform.position + cameraOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,(character.transform.position + cameraOffset), cameraSpeed*Time.deltaTime);
+
+    }
+}
