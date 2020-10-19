a6d261e72c66bf36ad00b08f40735388eac239ca
LeoIM <leo@leoim.com>
2020-10-18 19:51:38 -0700

    Patrol AI
    programmed a system for the AI to path between points set in editor. established a special gizmo to denote patrol pathing points.

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -11,6 +11,10 @@ public class EnemyAi : MonoBehaviour
 
     public LayerMask Floor, Whatplayer;
 
+    public Transform[] patrolPath;
+    //public float[] waitTimes;
+    private int currentPatrolPoint = 0;
+
     public Vector3 walkPoint;
     public Vector3 target;
     bool walkPointSet;
@@ -53,15 +57,11 @@ public class EnemyAi : MonoBehaviour
         {
             SearchWalkPoint();
         }
-
-        if (walkPointSet)
-        {
-            agent.SetDestination(walkPoint);
-        }
-
+        
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        distanceToWalkPoint.y = 0;
 
-        if (distanceToWalkPoint.magnitude < 1f)
+        if (distanceToWalkPoint.magnitude < .1f)
         {
             walkPointSet = false;
         }
@@ -70,16 +70,28 @@ public class EnemyAi : MonoBehaviour
 
     private void SearchWalkPoint()
     {
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, Floor))
-        {
-            walkPointSet = true;
+        if (currentPatrolPoint == patrolPath.Length) {
+            currentPatrolPoint = 0;
         }
+        walkPoint = patrolPath[currentPatrolPoint].position;
+        currentPatrolPoint++;
+        //yield return new WaitForSeconds(waitTimes[currentPatrolPoint]);
+
+        agent.SetDestination(walkPoint);
+
+        //float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        //float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        //walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        //if (Physics.Raycast(walkPoint, -transform.up, 2f, Floor))
+        //{
+        walkPointSet = true;
+        //}
     }
+
+
+
     private void ChasePlayer()
     {
         agent.SetDestination(player.position);
@@ -96,9 +108,9 @@ public class EnemyAi : MonoBehaviour
 
         if (!alreadyAttacked)
         {
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            //Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            //rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            //rb.AddForce(transform.up * 8f, ForceMode.Impulse);
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
