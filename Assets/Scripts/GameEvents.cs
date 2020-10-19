55b85b7bc5ac0fdd4ca2bf383761cb80fdd92ef3
Tim <timothy.seifert@tufts.edu>
2020-10-19 15:42:03 -0400

    Scene 1, restart to beginning if any scene is failed or health gets to zero

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameEvents : MonoBehaviour
 {
+    public Slider slider;
+    void Update()
+            {
+               if(slider.value == 0){
+                SceneManager.LoadScene("EnemyAiTest");
+                }
+            }
+
     void OnTriggerEnter(Collider other)
     {
     
         if (other.gameObject.tag == "Point")
         {
             Debug.Log("Collision");
-            SceneManager.LoadScene("EnemyAiTest");
+            SceneManager.LoadScene("Scene"+1);
         }
     }
 }
